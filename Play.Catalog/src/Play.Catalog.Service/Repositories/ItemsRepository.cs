3dbf6da97a25098be03708157e515772ca9f8cd2
Ivowainer <ivancamposwainer@gmail.com>
2024-01-30 12:26:03 -0300

    CATALOG - ItemRepository Verbs

--- Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
+++ Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MongoDB.Driver;
@@ -16,6 +17,46 @@ namespace Play.Catalog.Service.Repositories
             var mongoClient = new MongoClient("mongodb://localhost:27017");
             var database = mongoClient.GetDatabase("Catalog");
             dbCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<Item>> GetAllAsync()
+        {
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(item => item.Id, id);
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Item item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            await dbCollection.InsertOneAsync(item);
+        }
+
+        public async Task UpdateAsync(Item item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+
+            await dbCollection.ReplaceOneAsync(filter, item);
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(item => item.Id, id);
+
+            await dbCollection.DeleteOneAsync(filter);
         }
     }
 }
